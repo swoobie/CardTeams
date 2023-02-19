94936bdcd85bec684547f283c0a130f4b895cbd4
Swoobie <jdross91@gmail.com>
2023-02-19 16:54:56 -0700

    Clean up generated resources

---
+++ src/components/CardSelector.vue
@@ -0,0 +1,19 @@
+<script>
+
+
+
+export default {
+  data() {
+    return {
+        cardToTeamMapping:[] 
+    }
+  }
+}
+</script>
+
+<template>
+    <select v-model="selected">
+  <!-- inline object literal -->
+    <option :value="{ number: 123 }">123</option>
+    </select>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import CardSelector from "../components/CardSelector.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>This is a home page.</h1>
+    <CardSelector />
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
